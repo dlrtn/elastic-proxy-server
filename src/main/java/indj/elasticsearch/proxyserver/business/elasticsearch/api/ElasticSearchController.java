3d617929ae2ca0a2cdc3a0753ac9f0a99b818521
dlrtn <dlrtn751@gmail.com>
2022-08-29 19:02:06 +0900

    HttpMethod Get으로 변경 및 Controller 분리 후 pathVariable 기반 service parameter 추가

--- src/main/java/indj/elasticsearch/proxyserver/business/elasticsearch/api/ElasticSearchController.java
+++ src/main/java/indj/elasticsearch/proxyserver/business/elasticsearch/api/ElasticSearchController.java
@@ -3,23 +3,52 @@ package indj.elasticsearch.proxyserver.business.elasticsearch.api;
 import indj.elasticsearch.proxyserver.business.elasticsearch.application.ElasticSearchService;
 import indj.elasticsearch.proxyserver.business.elasticsearch.domain.payload.SearchRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/search")
 public class ElasticSearchController {
 
     private final ElasticSearchService searchService;
 
-    @PostMapping("/search")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public boolean searchStation(SearchRequest request) {
-        return searchService.searchData(request);
+    @GetMapping("/search_station")
+    public ResponseEntity<String> searchStation(SearchRequest request) {
+        String searchIndex = "search_station";
+        return searchService.searchData(request, searchIndex);
+    }
+
+    @GetMapping("/search_music_to_create")
+    public ResponseEntity<String> searchMusic(SearchRequest request) {
+        String searchIndex = "search_music_to_create";
+        return searchService.searchData(request, searchIndex);
+    }
+
+    @GetMapping("/search_Dj")
+    public ResponseEntity<String> searchDj(SearchRequest request) {
+        String searchIndex = "search_Dj";
+        return searchService.searchData(request, searchIndex);
+    }
+
+    @GetMapping("/search_Artist")
+    public ResponseEntity<String> searchArtist(SearchRequest request) {
+        String searchIndex = "search_Artist";
+        return searchService.searchData(request, searchIndex);
+    }
+
+    @GetMapping("/style_tag")
+    public ResponseEntity<String> searchStyleTag(SearchRequest request) {
+        String searchIndex = "style_tag";
+        return searchService.searchData(request, searchIndex);
+    }
+
+    @GetMapping("/search_tag")
+    public ResponseEntity<String> searchTag(SearchRequest request) {
+        String searchIndex = "search_tag";
+        return searchService.searchData(request, searchIndex);
     }
 
 }
